c0ca388a60e62ddeca7e54a71e47e3cf0015cb9c
silvervat <silver.vatsel@gmail.com>
2025-06-13 16:55:06 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Märgi saabunud extension - Trimble Connect 3D Workspace API näide
 
-// Utility: Kontrolli, kas kõik valitud detailid on juba "saabunud"
+// Kontrolli, kas kõik valitud detailid on juba "saabunud"
 async function allSelectedAreReceived(selection) {
   for (const object of selection) {
     const props = await window.TrimbleConnectWorkspace.getObjectProperties(object.id);
@@ -11,7 +11,7 @@ async function allSelectedAreReceived(selection) {
   return true;
 }
 
-// Utility: Märgi kõik valitud detailid "saabunuks"
+// Märgi kõik valitud detailid "saabunuks"
 async function markSelectedAsReceived(selection) {
   for (const object of selection) {
     let props = await window.TrimbleConnectWorkspace.getObjectProperties(object.id);
@@ -21,39 +21,75 @@ async function markSelectedAsReceived(selection) {
   }
 }
 
+// Kontrolli, kas kõik valitud detailid on juba "kontrollitud"
+async function allSelectedAreChecked(selection) {
+  for (const object of selection) {
+    const props = await window.TrimbleConnectWorkspace.getObjectProperties(object.id);
+    if (!props || props["Staatus"] !== "kontrollitud") {
+      return false;
+    }
+  }
+  return true;
+}
+
+// Märgi kõik valitud detailid "kontrollituks"
+async function markSelectedAsChecked(selection) {
+  for (const object of selection) {
+    let props = await window.TrimbleConnectWorkspace.getObjectProperties(object.id);
+    if (!props) props = {};
+    props["Staatus"] = "kontrollitud";
+    await window.TrimbleConnectWorkspace.setObjectProperties(object.id, props);
+  }
+}
+
 window.addEventListener('load', function() {
-  // Kuula valiku muutusi (ainult 3D vaates)
   window.TrimbleConnectWorkspace.on('selectionChanged', async function(selection) {
     const hasSelection = selection && selection.length > 0;
-    if (!hasSelection) {
-      window.TrimbleConnectWorkspace.setMenuButtonVisible('mark-received', false);
-      return;
-    }
-    // Kui kõik valitud on juba "saabunud", muuda nupu tekstiks "saabunud"
-    const alreadyReceived = await allSelectedAreReceived(selection);
-    if (alreadyReceived) {
-      window.TrimbleConnectWorkspace.setMenuButtonTitle('mark-received', 'saabunud');
-      window.TrimbleConnectWorkspace.setMenuButtonEnabled('mark-received', false);
+
+    // Märgi saabunud nupp
+    if (hasSelection) {
+      const alreadyReceived = await allSelectedAreReceived(selection);
+      if (alreadyReceived) {
+        window.TrimbleConnectWorkspace.setMenuButtonTitle('mark-received', 'saabunud');
+        window.TrimbleConnectWorkspace.setMenuButtonEnabled('mark-received', false);
+      } else {
+        window.TrimbleConnectWorkspace.setMenuButtonTitle('mark-received', 'MÄRGI SAABUNUD');
+        window.TrimbleConnectWorkspace.setMenuButtonEnabled('mark-received', true);
+      }
+      window.TrimbleConnectWorkspace.setMenuButtonVisible('mark-received', true);
+
+      // Märgi kontrollituks nupp
+      const alreadyChecked = await allSelectedAreChecked(selection);
+      if (alreadyChecked) {
+        window.TrimbleConnectWorkspace.setMenuButtonTitle('mark-checked', 'kontrollitud');
+        window.TrimbleConnectWorkspace.setMenuButtonEnabled('mark-checked', false);
+      } else {
+        window.TrimbleConnectWorkspace.setMenuButtonTitle('mark-checked', 'MÄRGI KONTROLLITUKS');
+        window.TrimbleConnectWorkspace.setMenuButtonEnabled('mark-checked', true);
+      }
+      window.TrimbleConnectWorkspace.setMenuButtonVisible('mark-checked', true);
+
     } else {
-      window.TrimbleConnectWorkspace.setMenuButtonTitle('mark-received', 'MÄRGI SAABUNUD');
-      window.TrimbleConnectWorkspace.setMenuButtonEnabled('mark-received', true);
+      window.TrimbleConnectWorkspace.setMenuButtonVisible('mark-received', false);
+      window.TrimbleConnectWorkspace.setMenuButtonVisible('mark-checked', false);
     }
-    window.TrimbleConnectWorkspace.setMenuButtonVisible('mark-received', true);
   });
 
-  // Kuula nupu vajutust
   window.TrimbleConnectWorkspace.on('menuClick', async function(event) {
-    if (event.id !== 'mark-received') return;
     const selection = await window.TrimbleConnectWorkspace.getSelection();
     if (!selection || selection.length === 0) return;
 
-    await markSelectedAsReceived(selection);
-
-    // Pärast muutmist kuva teade
-    window.TrimbleConnectWorkspace.showNotification('Valitud tooted saabunud!');
-
-    // Uuenda nuppude olekut pärast muutmist
-    window.TrimbleConnectWorkspace.setMenuButtonTitle('mark-received', 'saabunud');
-    window.TrimbleConnectWorkspace.setMenuButtonEnabled('mark-received', false);
+    if (event.id === 'mark-received') {
+      await markSelectedAsReceived(selection);
+      window.TrimbleConnectWorkspace.showNotification('Valitud tooted saabunud!');
+      window.TrimbleConnectWorkspace.setMenuButtonTitle('mark-received', 'saabunud');
+      window.TrimbleConnectWorkspace.setMenuButtonEnabled('mark-received', false);
+    }
+    if (event.id === 'mark-checked') {
+      await markSelectedAsChecked(selection);
+      window.TrimbleConnectWorkspace.showNotification('Valitud tooted kontrollitud!');
+      window.TrimbleConnectWorkspace.setMenuButtonTitle('mark-checked', 'kontrollitud');
+      window.TrimbleConnectWorkspace.setMenuButtonEnabled('mark-checked', false);
+    }
   });
 });
